0d77c7b522397aaf580908409812a8a3de24754d
Jiahe <yi.jiahe00@gmail.com>
2022-08-09 16:05:29 +0800

    draft out youtube client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use stream_chat_terminal::youtube_wrapper::Client;
+
 fn main() {
     println!("Hello, world!");
+
+    let yt = match Client::new() {
+        Ok(client) => client,
+        Err(err) => panic!("{}", err.to_string())
+    };
+
+    let messages = match yt.get_live_chat_messages("asdf") {
+        Ok(messages) => messages,
+        Err(err) => panic!("{}", err.to_string())
+    };
 }
---
+++ src/youtube_wrapper.rs
@@ -0,0 +1,42 @@
+use reqwest::header;
+
+const BASE_URL: &str = "https://www.googleapis.com/youtube/v3";
+
+pub struct Client {
+    key: String,
+    client: reqwest::blocking::Client,
+}
+
+impl Client {
+    pub fn new() -> Result<Client, String> {
+        let oauth2_token = ""; 
+        let header_value = format!("Bearer {}", oauth2_token);
+
+        let mut headers = header::HeaderMap::new();
+        headers.insert(header::AUTHORIZATION, header::HeaderValue::from_static(Box::leak(header_value.into_boxed_str())));
+
+        let client = match reqwest::blocking::Client::builder()
+            .build(){
+                Ok(client) => client,
+                Err(err) => return Err(err.to_string())
+            };
+
+        Ok(Client{
+            key: "".to_string(),
+            client
+        })
+    }
+
+    pub fn get_live_chat_messages(&self, live_chat_id: &str) -> Result<(), String> {
+        let res = match &self.client.get(format!("{}/liveChat/messages?liveChatId={}&part=snippet", BASE_URL, live_chat_id))
+            .send() {
+                Ok(res) => res,
+                Err(err) => return Err(err.to_string())
+            };
+
+        Ok(())
+    }
+    fn get_stream_id(&self) {
+
+    }
+}
