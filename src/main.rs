e613b018793932b12409c211a9c8e41a93f53146
Yi Jiahe <yi.jiahe00@gmail.com>
2022-08-12 10:20:22 +0800

    add user input for stream id

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 use stream_chat_terminal::youtube_wrapper::Client;
 
 fn main() {
@@ -8,8 +10,15 @@ fn main() {
         Err(err) => panic!("{}", err.to_string())
     };
 
-    let messages = match yt.get_live_chat_messages("asdf") {
+    let mut stream_id = String::new();
+
+    io::stdin()
+        .read_line(&mut stream_id)
+        .expect("Failed to read line");
+
+    let messages = match yt.get_live_chat_messages(&stream_id) {
         Ok(messages) => messages,
         Err(err) => panic!("{}", err.to_string())
     };
 }
+
